908c6cc19a757391fa5862bb2de5ded0bb56df55
Manodhar <manodhar2011@gmail.com>
2021-05-15 21:33:49 +0530

    this is by manodhar

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,11 +10,31 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira1");
 		System.out.println("postJira2");
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
 	}
 	
 	@Test
 	public void deleteTwitter()
 	{
-		System.out.println("deleteTwitter");	
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		
+		
+		
+
+		
+	}
+
+	private void deleteinsta() {
+		
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter");
+		
+		
+		// TODO Auto-generated method stub
+		
 	}
 }
